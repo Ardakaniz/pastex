ce6130b4e9c670786135ff5bb6b3e567ae5c1cd0
ardakaniz <ardakaniz@e.email>
2022-11-09 16:10:02 +0100

    Better seperation if elm files

--- elm/Ace.elm
+++ elm/Ace.elm
@@ -1,11 +1,35 @@
-module Ace exposing (Action(..), Pos, Msg, changeDecoder)
+module Ace exposing
+  ( Model
+  , Msg
+  , init
+  , update
+  , subscriptions
+  , view
+  , changeDecoder
+  )
+
+import Html exposing (Html, div)
+import Html.Attributes exposing (id)
 
 import Json.Encode exposing (Value)
 import Json.Decode as JD
 
+-- MODEL
+
+type alias Model = 
+  { content: String
+  }
+
+init : String -> Model
+init content =
+  { content = content }
+
+-- UPDATE 
+
 type Action
   = Insert
   | Remove
+  | Invalid
 
 type alias Pos =
   { row : Int
@@ -19,7 +43,94 @@ type alias Msg =
   , action : Action
   }
 
--- Json Decoders
+invalid_msg : Msg
+invalid_msg =
+  { start  = { row = 0, col = 0 }
+  , end    = { row = 0, col = 0 }
+  , lines  = [ ]
+  , action = Invalid
+  }
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg.action of
+    Insert ->
+      let new = insertAt (unlines msg.lines) msg.start.row msg.start.col model.content
+      in
+        { model | content = new }
+
+    Remove ->
+      let new = removeFromTo msg.start.row msg.start.col msg.end.row msg.end.col model.content
+      in
+        { model | content = new }
+
+    Invalid ->
+      model
+        
+-- SUBSCRIPTIONS
+
+subscriptions : (Value -> Msg)
+subscriptions = \val ->
+    JD.decodeValue changeDecoder val
+      |> Result.withDefault invalid_msg
+
+-- VIEW
+
+view : Html Msg
+view = div [ id "editor" ] []
+
+-- UTILS
+
+splitAt : Int -> List a -> ( List a, List a )
+splitAt i xs =
+  ( List.take i xs, List.drop i xs )
+
+unlines : List String -> String
+unlines = String.join "\n"
+
+insertAt : String -> Int -> Int -> String -> String
+insertAt src row col trgt =
+  case ( row, col ) of
+    ( 0, _ ) ->
+      let
+        ( before, after ) = splitAt col (String.toList trgt)
+      in
+        String.fromList before ++ src ++ String.fromList after
+
+    ( _, _ ) ->
+      case String.lines trgt of
+        x :: xs -> x ++ "\n" ++ (insertAt src (row - 1) col (unlines xs))
+        []      -> ""
+
+removeFromTo : Int -> Int -> Int -> Int -> String -> String
+removeFromTo fromr fromc tor toc trgt =
+  case ( fromr, fromc, tor ) of
+    ( 0, 0, 0 ) ->
+      String.dropLeft toc trgt
+    
+    ( 0, 0, _ ) ->
+      case String.lines trgt of
+        x :: xs -> removeFromTo 0 0 (tor - 1) toc (unlines xs)
+        []      -> ""
+
+    ( 0, _, _ ) ->
+      let
+        ( before, after ) = splitAt fromc (String.toList trgt)
+        new_toc =
+          if tor == 0
+          then toc - fromc
+          else toc
+      in
+        String.fromList before ++ (removeFromTo 0 0 tor new_toc (String.fromList after)) 
+
+    ( _, _, _ ) ->
+      case String.lines trgt of
+        x :: xs -> x ++ "\n" ++ (removeFromTo (fromr - 1) fromc (tor - 1) toc (unlines xs))
+        []      -> ""
+
+
+-- JSON DECODERS
+
 changeDecoder : JD.Decoder Msg
 changeDecoder =
   JD.map4 Msg
--- elm/Editor.elm
+++ elm/Editor.elm
@@ -3,13 +3,10 @@ port module Editor exposing (..)
 import Ace
 
 import Browser
-import Html exposing (Html, div, p, pre, text, button, input, label, br)
-import Html.Attributes exposing (class, id, type_, value, for)
-import Json.Decode as JD
-import Json.Encode as JE
-import Array
-import Html exposing (b)
-import Dict exposing (insert)
+import Html exposing (Html, div, p, pre, text, button, input, label, iframe, br)
+import Html.Attributes exposing (class, id, type_, value, for, src)
+import Html.Lazy exposing (lazy)
+import Json.Encode exposing (Value)
 
 -- MAIN
 
@@ -24,59 +21,38 @@ main =
 
 -- PORTS
 port toJS : String -> Cmd msg
-port fromJS : (JE.Value -> msg) -> Sub msg
+port fromJS : (Value -> msg) -> Sub msg
 
 -- MODEL
 
 type alias Model = 
-  { content: String
+  { ace : Ace.Model
   }
 
 init : String -> ( Model, Cmd Msg )
 init content =
-  ( { content = content }
+  ( { ace = Ace.init content }
   , toJS content
   )
 
 -- UPDATE
 
 type Msg
-  = OnAceChange Ace.Msg
-  | Nop
+  = OnAce Ace.Msg
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    OnAceChange ace_msg ->
-      case ace_msg.action of
-          Ace.Insert ->
-            let new = insertAt (unlines ace_msg.lines) ace_msg.start.row ace_msg.start.col model.content
-            in
-              ( { model | content = new }
-              , Cmd.none
-              )
-
-          Ace.Remove ->
-            let new = removeFromTo ace_msg.start.row ace_msg.start.col ace_msg.end.row ace_msg.end.col model.content
-            in
-              ( { model | content = new }
-              , Cmd.none
-              )
-
-    Nop ->
-      ( { model | content = "failed..." }
+    OnAce aceMsg ->
+      ( { model | ace = Ace.update aceMsg model.ace }
       , Cmd.none
       )
 
 -- SUBSCRIPTIONS
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    fromJS (\val ->
-      Result.withDefault Nop
-      <| Result.map OnAceChange
-      <| JD.decodeValue Ace.changeDecoder val
-    )
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Ace.subscriptions >> OnAce |> fromJS
 
 -- VIEW
 
@@ -84,7 +60,7 @@ view : Model -> Html Msg
 view model =
   div [ id "main-container" ]
     [ div [ class "panel" ] <| viewParameters model
-    , div [ id "editor"   ] []
+    , Html.map OnAce <| lazy (\_ -> Ace.view) ()
     , div [ class "panel" ] <| viewPdfPreview model
     ]
 
@@ -103,54 +79,5 @@ labeledInput ident inpType val =
 viewPdfPreview : Model -> List (Html Msg)
 viewPdfPreview _ =
   [ text "PDF Preview"
-  , div [ id "preview" ] []
+  , div [ id "preview" ] [ p [] [ text "Loading..." ] ]
   ]
-
--- UTILS
-
-takeDrop : Int -> List a -> ( List a, List a )
-takeDrop i xs =
-  ( List.take i xs, List.drop i xs )
-
-unlines : List String -> String
-unlines = String.join "\n"
-
-insertAt : String -> Int -> Int -> String -> String
-insertAt src row col trgt =
-  case ( row, col ) of
-    ( 0, _ ) ->
-      let
-        ( before, after ) = takeDrop col (String.toList trgt)
-      in
-        String.fromList before ++ src ++ String.fromList after
-
-    ( _, _ ) ->
-      case String.lines trgt of
-        x :: xs -> x ++ "\n" ++ (insertAt src (row - 1) col (unlines xs))
-        []      -> ""
-
-removeFromTo : Int -> Int -> Int -> Int -> String -> String
-removeFromTo fromr fromc tor toc trgt =
-  case ( fromr, fromc, tor ) of
-    ( 0, 0, 0 ) ->
-      String.dropLeft toc trgt
-    
-    ( 0, 0, _ ) ->
-      case String.lines trgt of
-        x :: xs -> removeFromTo 0 0 (tor - 1) toc (unlines xs)
-        []      -> ""
-
-    ( 0, _, _ ) ->
-      let
-        ( before, after ) = takeDrop fromc (String.toList trgt)
-        new_toc =
-          if tor == 0
-          then toc - fromc
-          else toc
-      in
-        String.fromList before ++ (removeFromTo 0 0 tor new_toc (String.fromList after)) 
-
-    ( _, _, _ ) ->
-      case String.lines trgt of
-        x :: xs -> x ++ "\n" ++ (removeFromTo (fromr - 1) fromc (tor - 1) toc (unlines xs))
-        []      -> ""
